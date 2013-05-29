a0c72c8ff1e9bd532445bdeaf11996a7128e02cf
Trevor Redfern <trevorredfern@gmail.com>
2013-05-29 13:27:21 -0700

    Changed it so it shows the RVM ruby version and gemset in the prompt

--- gitprompt.sh
+++ gitprompt.sh
@@ -90,19 +90,24 @@ function setGitPrompt() {
 	  fi
 	  STATUS="$STATUS$ResetColor$GIT_PROMPT_SUFFIX"
 
-	  PS1="$PYTHON_VIRTUALENV$PROMPT_START$STATUS$PROMPT_END"
+	  PS1="$VIRTUALENVIRONMENT$PROMPT_START$STATUS$PROMPT_END"
 	else
 	  PS1="$PROMPT_START$PROMPT_END"
 	fi
 }
 
+function __my_rvm_ruby_version {
+  local gemset=$(echo $GEM_HOME | awk -F'@' '{print $2}')
+  [ "$gemset" != "" ] && gemset="@$gemset"
+  local version=$(echo $MY_RUBY_HOME | awk -F'-' '{print $2}')
+  [ "$version" == "1.8.7" ] && version=""
+  local full="$version$gemset"
+  [ "$full" != "" ] && echo "$full "
+}
+
 # Determine active Python virtualenv details.
 function set_virtualenv () {
-  if test -z "$VIRTUAL_ENV" ; then
-      PYTHON_VIRTUALENV=""
-  else
-      PYTHON_VIRTUALENV="${BLUE}(`basename \"$VIRTUAL_ENV\"`)${ResetColor} "
-  fi
+  VIRTUALENVIRONMENT="${Blue}ruby-$(__my_rvm_ruby_version)${ResetColor} " 
 }
 
 PROMPT_COMMAND=setGitPrompt
